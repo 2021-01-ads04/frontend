3ad4794bd93244d59039acad472cd8fdc3b38a22
Marcospaulo-paixao <contato.marcospaulo1@gmail.com>
2021-03-08 21:45:14 -0300

    angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { DataService } from './service/data.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
